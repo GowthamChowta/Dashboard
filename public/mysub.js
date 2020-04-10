1d5d1a0cb16ced794b43f3458de9541b91ca3243
Gowtham <gautigadu091@gmail.com>
2020-04-10 12:32:50 +0530

    Updated JS code

--- public/mysub.js
+++
@@ -1,8 +0,0 @@
-if($('td').length ===0){
-    $('table').addClass('hidden')
-}
-else
-{
-  $('table').removeClass('hidden')
-  // $('h3.message')[0].innerHTML="Please find the submission for the assignment"
-}
